6124c654df898eaa931475ceeddcfdf330877c4c
Devon Walker <devonw@andrew.cmu.edu>
2016-07-08 13:09:13 -0400

    Remove 2d structure specific methods

--- devon_toolbox.py
+++ devon_toolbox.py
@@ -45,37 +45,6 @@ def is_the_same(x, fun, *args):
     return x == y
 
 
-def get_neighbors(atoms, index, layer, cutoff=4.0):
-    """Return neighbor indices of the atom at index for a cutoff distance.
-
-    Determines a list of neighboring atoms to the index atom. It uses a cutoff distance to determine the absolute distance away from an atom that would constitute it as a neighbor.
-
-    Args:
-        atoms (Atoms): Cell of atoms with multiple layers of a 2D material.
-        index (int): atoms[index] is the atom used to find its neighbors.
-        layer (List[int]): Indices of atoms in the layer of question.
-        cutoff (float): If an atom's position away is less than cutoff, it is a neighbor.
-        
-    Returns:
-        A list of indices (int) of neighbors in atoms.
-    """
-    neighbors = []
-    pos = atoms[index].position
-    layer_atoms = [a for a in atoms if a.index in layer]
-    for a in layer_atoms:
-        dist = np.linalg.norm(a.position - pos)
-        if dist <= cutoff and dist > 0.001:
-            neighbors.append(a.index)
-
-    return neighbors
-
-
-def make_pore(atoms, indices):
-    """Delete atoms at indices to create a pore."""
-    for index in sorted(indices, reverse=True):
-        del atoms[index]
-
-
 def paint_atoms(atoms, indices, sym=None, layers=None):
     """Update the chemical symbol of atoms in the list of indices."""
     if sym is not None:
@@ -137,81 +106,6 @@ def status_inprogress():
 
 def status_unconverged(i):
     print("Distance: {:5.2f}. Did not converge.".format(i))
-
-
-def structure(atoms, layers=1, molecs=0, thresh=2.0):
-    """Return lists of the indices of different structures in a unitcell.
-    
-    Specifically used for my 2D material transport structures to retrieve layers and molecules. Starts from height of z=0 and moves upwards. Note: A k-means algorithm could work well here, but the one currently (2016-06-06) in scipy did not work reliably here due to some randomness. It may be worthwhile to look into it more if we want to define molecules in 3D space.
-
-    Args:
-        atoms (Atoms): Unitcell of atoms
-        layers (int): Number of layers
-        molecs (int): Number of molecules. 0 or 1. >1 not implemented.
-        thresh (float): Total height of a layer.
-
-    Returns:
-        Dict containing indices of atoms representing layers and molecules.
-        
-        Example:
-        {'layers': [[0, 1, 4, 5], [2, 3, 6, 7]]
-         'molecs': [8,9]}
-    """
-    structure = {}
-    structure['layers'] = []
-    structure['molecs'] = []
-    unaccounted = [atom for atom in atoms]
-    
-    for layer in range(layers):
-        anchor = closest_atom_to_height(unaccounted, 0)
-
-        group = []
-        for atom in unaccounted:
-            dist = abs(anchor.position[2] - atom.position[2])
-            if dist <= thresh:
-                group.append(atom)
-
-
-        indices = [g.index for g in group]
-        structure['layers'].append(indices)
-        unaccounted = [a for a in unaccounted if a.index not in indices]
-    
-    if molecs > 0:
-        structure['molecs'].append([a.index for a in unaccounted])
-
-    return structure
-
-
-def closest_atom(atoms, position, exclude=None):
-    """Return the index of the atom closest to a position."""
-    choices = [a.index for a in atoms]
-
-    if exclude is not None:
-        choices = [i for i in choices if i not in exclude]
-
-    closest = None
-    min_dist = None
-    for i in choices:
-        dist = np.linalg.norm(atoms[i].position - position)
-        if np.absolute(dist) < min_dist or closest is None:
-            min_dist = dist
-            closest = i
-
-    return closest
-
-
-def closest_atom_to_height(atoms, height):
-    """Return the first atom closest to height in the z-direction."""
-    closest = atoms[0]
-    min_dist = abs(height - closest.position[2])
-
-    for atom in atoms:
-        dist = abs(height - atom.position[2])
-        if dist < min_dist:
-            closest = atom
-            min_dist = dist
-
-    return closest
 
 
 def spline(x, y, points=200):
